327707101944c935488a3bae45a97b1fc90c7804
Daniel Hjartland <squashical@gmail.com>
2023-05-21 12:06:59 +0200

    Reorganize docs

--- config/docs.ts
+++ config/docs.ts
@@ -3,13 +3,13 @@ import { DocsConfig } from "types";
 export const docsConfig: DocsConfig = {
   mainNav: [
     {
+      title: "Projects",
+      href: "/projects",
+    },
+    {
       title: "Documentation",
       href: "/docs",
     },
-    // {
-    //   title: "Guides",
-    //   href: "/guides",
-    // },
   ],
   sidebarNav: [
     {
@@ -29,29 +29,163 @@ export const docsConfig: DocsConfig = {
           href: "/docs/documentation",
         },
         {
-          title: "Curriculum",
+          title: "Curriculum / Projects",
           href: "/docs/documentation/curriculum",
         },
       ],
     },
     {
-      title: "Projects",
+      title: "Foundations",
       items: [
         {
           title: "Introduction",
-          href: "/docs/projects",
+          href: "/docs/foundations",
         },
         {
           title: "Recipes",
-          href: "/docs/projects/recipes",
+          href: "/docs/foundations/recipes",
         },
         {
           title: "Landing Page",
-          href: "/docs/projects/landing-page",
+          href: "/docs/foundations/landing-page",
         },
         {
           title: "Rock Paper Scissors",
-          href: "/docs/projects/rock-paper-scissors",
+          href: "/docs/foundations/rock-paper-scissors",
+        },
+        {
+          title: "Etch a Sketch",
+          href: "/docs/foundations/etch-a-sketch",
+          disabled: true,
+        },
+        {
+          title: "Calculator",
+          href: "/docs/foundations/calculator",
+          disabled: true,
+        },
+      ],
+    },
+    {
+      title: "Intermediate HTML/CSS",
+      items: [
+        {
+          title: "Signup Form",
+          href: "/docs/intermediate-html-css/signup-form",
+          disabled: true,
+        },
+        {
+          title: "Admin Dashboard",
+          href: "/docs/intermediate-html-css/admin-dashboard",
+          disabled: true,
+        },
+      ],
+    },
+    {
+      title: "JavaScript",
+      items: [
+        {
+          title: "Library",
+          href: "/docs/javascript/library",
+          disabled: true,
+        },
+        {
+          title: "Tic Tac Toe",
+          href: "/docs/javascript/tic-tac-toe",
+          disabled: true,
+        },
+        {
+          title: "Restaurant Page",
+          href: "/docs/javascript/restaurant-page",
+          disabled: true,
+        },
+        {
+          title: "Todo List",
+          href: "/docs/javascript/todo-list",
+          disabled: true,
+        },
+        {
+          title: "Weather App",
+          href: "/docs/javascript/weather-app",
+          disabled: true,
+        },
+        {
+          title: "Data Structures and Algorithms",
+          href: "/docs/javascript/data-structures-and-algorithms",
+          disabled: true,
+        },
+        {
+          title: "Battle Ship",
+          href: "/docs/javascript/battle-ship",
+          disabled: true,
+        },
+        {
+          title: "CV Builder",
+          href: "/docs/javascript/cv-builder",
+          disabled: true,
+        },
+        {
+          title: "Memory Card Game",
+          href: "/docs/javascript/memory-card-game",
+          disabled: true,
+        },
+        {
+          title: "Shopping Cart",
+          href: "/docs/javascript/shopping-cart",
+          disabled: true,
+        },
+        {
+          title: "Where's Waldo",
+          href: "/docs/javascript/wheres-waldo",
+          disabled: true,
+        },
+        {
+          title: "Final Project",
+          href: "/docs/javascript/final-project",
+          disabled: true,
+        },
+        {
+          title: "Personal Portfolio – live",
+          href: "https://hjartland.dev",
+        },
+      ],
+    },
+    {
+      title: "NodeJS",
+      items: [
+        {
+          title: "Basic Informational Site",
+          href: "/docs/nodejs/basic-informational-site",
+          disabled: true,
+        },
+        {
+          title: "Forms and Deployment",
+          href: "/docs/nodejs/forms-and-deployment",
+          disabled: true,
+        },
+        {
+          title: "Mini Message Board",
+          href: "/docs/nodejs/mini-message-board",
+          disabled: true,
+        },
+        {
+          title: "Inventory Application",
+          href: "/docs/nodejs/inventory-application",
+          disabled: true,
+        },
+        {
+          title: "Members Only",
+          href: "/docs/nodejs/members-only",
+          disabled: true,
+        },
+        {
+          title: "Blog API",
+          href: "/docs/nodejs/blog-api",
+          disabled: true,
+        },
+        {
+          title: "Odin Book",
+          href: "/docs/nodejs/odin-book",
+          disabled: true,
         },
       ],
     },
